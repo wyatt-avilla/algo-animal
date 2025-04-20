c3fc9bead538e931be1967d5cd24d3183238afc2
“Miguel <erqmac@gmail.com>
2025-04-19 17:55:41 -0700

    pweaseWork

--- frontend/src/AnimalTings/animalSpritesRef.js
+++ frontend/src/AnimalTings/animalSpritesRef.js
@@ -1,50 +1,34 @@
 class animalSpriteRef extends Phaser.Scene {
-
-    constructor (){
-        super('animalSpriteRef')
+    constructor() {
+        super('animalSpriteRef');
     }
 
     preload() {
-
-        // Load the panda sprite sheets 
-        this.load.spritesheet('pandaHappy', 'frontend/src/AnimalTings/AnimalAssets/Happy.png', {frameWidth: 64, frameHeight: 64})
-        this.load.spritesheet('pandaCry', 'frontend/src/AnimalTings/AnimalAssets/PandaCry.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaEating', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaIdle', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaIdleBlinking', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaResting', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaSitting', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaSleep', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaSoFull', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaTalkingSitting', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaThinking', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-        //this.load.spritesheet('pandaWave', 'frontend/src/AnimalTings/AnimalAssets/animal.png', {frameWidth: 64, frameHeight: 64})
-
-        // Load other sprite sheets later
-
+        // Load the panda sprite sheets
+        this.load.spritesheet('pandaHappy', '/src/AnimalTings/AnimalAssets/PandaSprite/Happy.png', {frameWidth: 64, frameHeight: 64});
+        this.load.spritesheet('pandaCry', '/src/AnimalTings/AnimalAssets/PandaSprite/PandaCry.png', {frameWidth: 64, frameHeight: 64});
+        
         // Debug tool
         this.textures.on('addtexture', (key) => {
-            console.log(`Texture loaded: ${key}`)
+            console.log(`Texture loaded: ${key}`);
         });
-
     }
 
     create() {
-        //this.add.sprite(100, 100, 'pandaHappy').setOrigin(0.5, 0.5).setScale(2);
-
+        // Create a sprite to display the animation
+        this.player = this.add.sprite(100, 100, 'pandaHappy');
+        
+        // Create the animation
         this.anims.create({
             key: 'pandaHappy',
             frames: this.anims.generateFrameNumbers('pandaHappy', { start: 0, end: 3 }),
             frameRate: 10,
             repeat: -1
-        })
-
-
+        });
+        
+        // Play the animation
         this.player.play('pandaHappy').setOrigin(0.5, 0.5).setScale(2);
     }
-
-
 }
 
 export default animalSpriteRef;
-
